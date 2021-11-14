c36f6333d24915ca2975f4e32d017b16f7489e3c
chi cong vu <Chicong422002@gmail.com>
2021-11-14 23:44:04 +0700

    fixing button function
    done

--- data-structure-visualization/src/types.js
+++ data-structure-visualization/src/types.js
@@ -1,26 +1,27 @@
 import React, { useEffect, useState } from "react";
 
-const typeList = ["array", "linked list"];
+const typeList = ["array", "linked list", "binary tree"];
 const TypeBtn = () => {
   const [types, setTypes] = useState([]);
   const [currentType, setCurrentType] = useState(typeList[0]);
   if (typeList.length > types.length) {
     setTypes(typeList);
   }
-  useEffect(() => {
-    const getRoot = document.getElementById("root");
-  }, []);
   const activateBtn = (type) => {
+    const getRoot = document.getElementById("root");
     const getAllBtn = getRoot.querySelectorAll(".typeBtn");
     setCurrentType(type);
     getAllBtn.forEach((btn) => {
-      if (btn.getAttribute === currentType) {
+      if (btn.getAttribute("id") === currentType) {
         btn.classList.add("activatedBtn");
       } else {
         btn.classList.remove("activatedBtn");
       }
     });
+    console.log(currentType);
+    console.log(getAllBtn);
   };
+  useEffect(() => activateBtn(currentType));
   return (
     <div className="typeBtns">
       {types.map((type) => {
